f9f5798da6bbcf4c4571d5758f732d81fd093d27
ViRuS007 <virus007@protonmail.com>
2017-01-05 22:55:27 +0330

    updated

--- lib/recive_data.rb
+++ lib/recive_data.rb
@@ -1,6 +1,7 @@
-#!/usr/bin/ruby
+#!/usr/bin/rubyS
 
 require "json"
+require "colorize"
 
 class Recive_data
 	#def generalstat
@@ -8,12 +9,23 @@ class Recive_data
 		host = "http://bestoon.ir"
 		#token
 		token = "kgfqN5pRwNYXghqYeUdFHntRyisWTshYvylRko9E8TZeMtqU"
-		#income bestton api directory
-		income = "/submit/income/ "
 		#generalstate bestoon api directory
 		generalstate = "/q/generalstat/"
 		#send data from curl linux
-	  system("curl --data \"token=#{token}\" #{host}#{generalstate} > .json_file/generalstate.json")
+	  system("curl --data \"token=#{token}\" #{host}#{generalstate} > json_file/generalstate.json")
+		#Reading json file
+		file = File.open("json_file/generalstate.json")
+		json = file.read
+		#paes json
+		parsed = JSON.parse(json)
+		parsed["expense"].each do |s|
+			print "\n\nExpense: "
+			p s[1]
+		end
+		parsed["income"].each do |i|
+			print "Income: "
+			p i[1]
+		end
 	end
 	#def expense
 	def expense
@@ -28,6 +40,12 @@ class Recive_data
 		dec = gets.chomp
 		#send data from curl linux
 		system("curl --data \"token=#{token}&amount=#{am}&text=#{dec}\" #{host}#{expense} > json_file/expense.json")
+
+		print "\nDo you want to show all The sum of all transactions?(y,n)"
+		sum = gets.chomp
+		if sum == "y"
+			generalstat
+		end
 	end
 	#def income
 	def income
@@ -41,5 +59,10 @@ class Recive_data
 		dec = gets.chomp
 		#send data from curl linux
 		system("curl --data \"token=#{token}&amount=#{inc}&text=#{dec}\" #{host}#{income} > json_file/income.json")
+		print "\nDo you want to show all The sum of all transactions?(y,n)"
+		sum = gets.chomp
+		if sum == "y"
+			generalstat
+		end
 	end
 end
